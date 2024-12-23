e0bb3e8d8cb2bde4dd5715df21ddc55802f4126a
mdsaket-dev <mohammed.saket@permi.tech>
2024-12-23 19:19:29 +0530

    Update design system: Bump version to 0.0.13, refactor FloatingToolbar component to use named export, and update imports in related components for consistency.

--- design-system/src/components/FloatingToolbar/FloatingToolbar.stories.tsx
+++ design-system/src/components/FloatingToolbar/FloatingToolbar.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-import FloatingToolbar from "./FloatingToolbar";
+import { FloatingToolbar } from "./FloatingToolbar";
 
 const meta: Meta<typeof FloatingToolbar> = {
   component: FloatingToolbar,
